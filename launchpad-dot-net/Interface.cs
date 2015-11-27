a7c938c676c4b4b2152ffa1d98ada6aca5140e8f
iUltimateLP <iredcraft@gmail.com>
2015-11-27 17:14:38 +0100

    First working commit
    First working stuff :+1:

---
+++ launchpad-dot-net/Interface.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Midi;
+
+namespace LaunchpadNET
+{
+    public class Interface
+    {
+        private Pitch[,] notes = new Pitch[8, 8] {
+            { Pitch.A5, Pitch.ASharp5, Pitch.B5, Pitch.C6, Pitch.CSharp6, Pitch.D6, Pitch.DSharp6, Pitch.E6 },
+            { Pitch.B4, Pitch.C5, Pitch.CSharp5, Pitch.D5, Pitch.DSharp5, Pitch.E5, Pitch.F5, Pitch.FSharp5 },
+            { Pitch.CSharp4, Pitch.D4, Pitch.DSharp4, Pitch.E4, Pitch.F4, Pitch.FSharp4, Pitch.G4, Pitch.GSharp4 },
+            { Pitch.DSharp3, Pitch.E3, Pitch.F3, Pitch.FSharp3, Pitch.G3, Pitch.GSharp3, Pitch.A3, Pitch.ASharp3 },
+            { Pitch.F2, Pitch.FSharp2, Pitch.G2, Pitch.GSharp2, Pitch.A2, Pitch.ASharp2, Pitch.B2, Pitch.C3 },
+            { Pitch.G1, Pitch.GSharp1, Pitch.A1, Pitch.ASharp1, Pitch.B1, Pitch.C2, Pitch.CSharp2, Pitch.D2 },
+            { Pitch.A0, Pitch.ASharp0, Pitch.B0, Pitch.C1, Pitch.CSharp1, Pitch.D1, Pitch.DSharp1, Pitch.E1 },
+            { Pitch.BNeg1, Pitch.C0, Pitch.CSharp0, Pitch.D0, Pitch.DSharp0, Pitch.E0, Pitch.F0, Pitch.FSharp0 }
+        };
+
+        public InputDevice targetInput;
+        public OutputDevice targetOutput;
+
+        public delegate void LaunchpadKeyEventHandler(object source, LaunchpadKeyEventArgs e);
+
+        /// <summary>
+        /// Event Handler when a Launchpad Key is pressed.
+        /// </summary>
+        public event LaunchpadKeyEventHandler OnLaunchpadKeyPressed;
+
+        /// <summary>
+        /// EventArgs for pressed Launchpad Key
+        /// </summary>
+        public class LaunchpadKeyEventArgs : EventArgs
+        {
+            private int x;
+            private int y;
+            public LaunchpadKeyEventArgs(int _pX, int _pY)
+            {
+                x = _pX;
+                y = _pY;
+            }
+            public int GetX()
+            {
+                return x;
+            }
+            public int GetY()
+            {
+                return y;
+            }
+        }
+
+        private void midiPress(Midi.NoteOnMessage msg)
+        {
+            if (OnLaunchpadKeyPressed != null)
+            {
+                OnLaunchpadKeyPressed(this, new LaunchpadKeyEventArgs(midiNoteToLed(msg.Pitch)[0], midiNoteToLed(msg.Pitch)[1]));
+            }
+        }
+
+        /// <summary>
+        /// Returns the LED coordinates of a MIdi note
+        /// </summary>
+        /// <param name="p">The Midi Note.</param>
+        /// <returns>The X,Y coordinates.</returns>
+        public int[] midiNoteToLed(Pitch p)
+        {
+            for (int x = 0; x <= 7; x++)
+            {
+                for (int y = 0; y <= 7; y++)
+                {
+                    if (notes[x,y] == p)
+                    {
+                        int[] r1 = { x, y };
+                        return r1;
+                    }
+                }
+            }
+            int[] r2 = { 0, 0 };
+            return r2;
+        }
+
+        /// <summary>
+        /// Returns the equilavent Midi Note to X and Y coordinates.
+        /// </summary>
+        /// <param name="x">The X coordinate of the LED</param>
+        /// <param name="y">The Y coordinate of the LED</param>
+        /// <returns>The midi note</returns>
+        public Pitch ledToMidiNote(int x, int y)
+        {
+            return notes[x, y];
+        }
+
+        public void clearAllLEDs()
+        {
+            for (int x = 0; x < 8; x++)
+            {
+                for (int y = 0; y < 8; y++)
+                {
+                    setLED(x, y, 0);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Creates a rectangular mesh of LEDs.
+        /// </summary>
+        /// <param name="startX">Start X coordinate</param>
+        /// <param name="startY">Start Y coordinate</param>
+        /// <param name="endX">End X coordinate</param>
+        /// <param name="endY">End Y coordinate</param>
+        /// <param name="velo">Painting velocity</param>
+        public void fillLEDs(int startX, int startY, int endX, int endY, int velo)
+        {
+            Pitch startPitch = notes[startX, startY];
+            Pitch endPitch = notes[endX, endY];
+
+            for (int x = 0; x < notes.Length; x++)
+            {
+                for (int y = 0; y < notes.Length; y++)
+                {
+                    if (x >= startX && y >= startY && x <= endX && y <= endY)
+                        setLED(x, y, velo);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Sets a LED of the Launchpad.
+        /// </summary>
+        /// <param name="x">The X coordinate.</param>
+        /// <param name="y">The Y coordinate.</param>
+        /// <param name="velo">The velocity.</param>
+        public void setLED(int x, int y, int velo)
+        {
+            try
+            {
+                targetOutput.SendNoteOn(Channel.Channel1, notes[x, y], velo);
+            }
+            catch (Midi.DeviceException)
+            {
+                Console.WriteLine("<< LAUNCHPAD.NET >> Midi.DeviceException");
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Returns all connected and installed Launchpads.
+        /// </summary>
+        /// <returns>Returns LaunchpadDevice array.</returns>
+        public LaunchpadDevice[] getConnectedLaunchpads()
+        {
+            List<LaunchpadDevice> tempDevices = new List<LaunchpadDevice>();
+
+            foreach (InputDevice id in Midi.InputDevice.InstalledDevices)
+            {
+                foreach (OutputDevice od in Midi.OutputDevice.InstalledDevices)
+                {
+                    if (id.Name == od.Name)
+                    {
+                        if (id.Name.ToLower().Contains("launchpad"))
+                        {
+                            tempDevices.Add(new LaunchpadDevice(id.Name));
+                        }
+                    }
+                }
+            }
+
+            return tempDevices.ToArray();
+        }
+
+        /// <summary>
+        /// Function to connect with a LaunchpadDevice
+        /// </summary>
+        /// <param name="device">The Launchpad to connect to.</param>
+        /// <returns>Returns bool if connection was successful.</returns>
+        public bool connect(LaunchpadDevice device)
+        {
+            Console.WriteLine("Connecting");
+            foreach(InputDevice id in Midi.InputDevice.InstalledDevices)
+            {
+                Console.WriteLine("id.Name = " + id.Name.ToLower() + " device._midiName = " + device._midiName);
+                if (id.Name.ToLower() == device._midiName.ToLower())
+                {
+                    targetInput = id;
+                    Console.WriteLine("Opening " + device._midiName);
+                    id.Open();
+                    targetInput.NoteOn += new InputDevice.NoteOnHandler(midiPress);
+                    targetInput.StartReceiving(null);
+                }
+            }
+            foreach (OutputDevice od in Midi.OutputDevice.InstalledDevices)
+            {
+                if (od.Name.ToLower() == device._midiName.ToLower())
+                {
+                    targetOutput = od;
+                    Console.WriteLine("Opening " + device._midiName);
+                    od.Open();
+                }
+            }
+
+            return true; // targetInput.IsOpen && targetOutput.IsOpen;
+        }
+
+        /// <summary>
+        /// Disconnects a given LaunchpadDevice
+        /// </summary>
+        /// <param name="device">The Launchpad to disconnect.</param>
+        /// <returns>Returns bool if disconnection was successful.</returns>
+        public bool disconnect(LaunchpadDevice device)
+        {
+            if (targetInput.IsOpen && targetOutput.IsOpen)
+            {
+                targetInput.StopReceiving();
+                targetInput.Close();
+                targetOutput.Close();
+            }
+            return !targetInput.IsOpen && !targetOutput.IsOpen;
+        }
+
+        public class LaunchpadDevice
+        {
+            public string _midiName;
+            //public int _midiDeviceId;
+
+            public LaunchpadDevice(string name)
+            {
+                _midiName = name;
+            }
+        }
+    }
+}
